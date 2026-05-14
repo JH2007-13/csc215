59be419972c0d79f8a2e936ab2f7ed70f5de31cd
JH2007-13 <jrharlva@gmail.com>
2026-05-14 06:00:04 -0400

    Update shellshort.h

--- shellshort.h
+++ shellshort.h
@@ -8,7 +8,7 @@ int n;
   while (flag == 1 || gap > 1) //Ensures that even if the gap is 1, the sort continues until fully sorted
   {
     flag = 0;
-    gap = gap/2; //Gap starts at half the length of the array
+    gap = gap/2; //Gap starts at half the length of the array, and halves itself again each cycle
     for (i = 0; i < n-gap; i++) //Halts each sorting cycle before it goes over the edge of the array
     {
       if (arr[i+gap] < arr[i]) //If the second value is smaller than the first value...
