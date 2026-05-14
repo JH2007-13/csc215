d755536ad1753a99117506b12f00e677d67f258d
JH2007-13 <jrharlva@gmail.com>
2026-05-14 05:58:40 -0400

    Update shellshort.h

--- shellshort.h
+++ shellshort.h
@@ -4,17 +4,17 @@ shell_sort(arr, n)
 int arr[];
 int n;
 {
-  int i, gap = 2, flag = 1, arrL = n;
-  while (flag == 1 || gap > 1)
+  int i, flag = 1, gap = n+1;
+  while (flag == 1 || gap > 1) //Ensures that even if the gap is 1, the sort continues until fully sorted
   {
     flag = 0;
-    gap = arrL/2;
-    for (i = 0; i < arrL-gap; i++)
+    gap = gap/2; //Gap starts at half the length of the array
+    for (i = 0; i < n-gap; i++) //Halts each sorting cycle before it goes over the edge of the array
     {
-      if (arr[i+gap] < arr[i])
+      if (arr[i+gap] < arr[i]) //If the second value is smaller than the first value...
       {
-        swap(&arr[i+gap],&arr[i]);
-        flag = 1;
+        swap(&arr[i+gap],&arr[i]); //Both values are swapped
+        flag = 1; //Everytime a swap happens, the flag is triggered to ensure the program runs again
       }
     }
   }
