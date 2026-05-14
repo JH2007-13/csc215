c4c1682d913eee928e71b079d6064dd002e34de5
JH2007-13 <jrharlva@gmail.com>
2026-05-14 04:49:35 -0400

    Add shell sort implementation in shellshort.c
    Implement shell sort algorithm for sorting an array.

--- shellshort.c
+++ shellshort.c
@@ -1,1 +1,21 @@
 #include <stdio.h>
+
+shell_sort(arr, n)
+int arr[];
+int n;
+{
+  int i, gap = 2, flag = 1, arrL = n;
+  while (flag == 1 || gap > 1)
+  {
+    flag = 0;
+    gap = arrL/2;
+    for (i = 0; i < arrL-gap; i++)
+    {
+      if (arr[i+gap] < arr[i])
+      {
+        swap(&arr[i+gap],&arr[i]);
+        flag = 1;
+      }
+    }
+  }
+}
