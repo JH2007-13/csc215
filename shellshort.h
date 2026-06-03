affcc89b2b07a0541ac084d961a42120bedde4f7
JH2007-13 <jrharlva@gmail.com>
2026-06-03 09:10:18 -0400

    Update shellshort.h

--- shellshort.h
+++ shellshort.h
@@ -17,21 +17,21 @@ int shell_sort(arr, n)
 int arr[];
 int n;
 {
-  int i, j, temp, cycles, gap;
-  cycles = 0;
-  for (gap = n / 2; gap > 0; gap = gap / 2)
+  int i, j, temp, cycles, gap; //Variable initializer
+  cycles = 0; //Cycles variable tracks how many cycles the function takes (for efficiency)
+  for (gap = n / 2; gap > 0; gap = gap / 2) //Gap loop starts the gap at half the length of the array, and halves it every cycle
   {
     cycles += 1;
-    for (i = gap; i < n; i++)
+    for (i = gap; i < n; i++) //Starts the comparison algorithm at the end of the first gap, and then moves right until the end of the array
     {
-      temp = arr[i];
+      temp = arr[i]; //Initial value is stored in a temporary variable (for insertion later)
       j = i;
-      while (j >= gap && arr[j - gap] > temp)
+      while (j >= gap && arr[j - gap] > temp) //Compares the target value with different places along the gap trajectory
       {
-        arr[j] = arr[j - gap];
-        j = j - gap;
+        arr[j] = arr[j - gap]; //Swaps the current value with an earlier value along the gap trajectory if necessary
+        j = j - gap; //Moves the target back one space along the gap trajectory
       }
-      arr[j] = temp;
+      arr[j] = temp; //Initial variable is dropped into its place
     }
   }   
   return cycles;
